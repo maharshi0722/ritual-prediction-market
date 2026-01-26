ba04a4325da6dd978e0d7c84e5acfd1921e4b8b1
maharshi <maharshi621@gmail.com>
2026-01-26 23:39:15 +0530

    final done heeee

--- app/api/suggestions/[id]/approve/route.js
+++ app/api/suggestions/[id]/approve/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "../../../../lib/db";
-import Suggestion from "../../../../models/suggestion";
+
+import { connectDB } from "../../../../../lib/db";
+import Suggestion from "../../../../../models/suggestion";
 import Prediction from "../../../../../models/prediction";
-import { getUserId } from "../../../../lib/getUser";
-import { broadcast } from "../../../../../../lib/sseStore";
+import { getUserId } from "../../../../../lib/getUser";
+import { broadcast } from "../../../../../lib/sseStore";
+
  // send to SSE clients
 
 // POST /api/suggestions/:id/approve  — only admin/curator should call this
